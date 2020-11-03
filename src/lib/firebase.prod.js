711dd482abd0a9aee90ad40c0cb28f2d0f3a7809
vahan akopjanian <vahan.bclub@gmail.com>
2020-11-03 12:55:41 +0200

    add firebase context, provider

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,9 +4,19 @@ import "firebase/auth";
 
 //here we need to seed our database
 
-//we also need config
-const config = {};
+//we also need config form firebase
+const config = {
+  apiKey: "AIzaSyBeLxGv5SJ5QnP9891YhcniJ65pHcoaBlQ",
+  authDomain: "netflix-clone-82741.firebaseapp.com",
+  databaseURL: "https://netflix-clone-82741.firebaseio.com",
+  projectId: "netflix-clone-82741",
+  storageBucket: "netflix-clone-82741.appspot.com",
+  messagingSenderId: "719772479208",
+  appId: "1:719772479208:web:abc8db1af17a00e700635d",
+};
 
 const firebase = Firebase.initializeApp(config);
+
+//seedDatabase(firebase);
 
 export { firebase };
