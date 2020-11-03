c72721673655fe9e9288da108f545449d61f7caa
vahan akopjanian <vahan.bclub@gmail.com>
2020-11-03 19:57:59 +0200

    add redirection and protected routes

---
+++ src/helpers/routes.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+//here we have functions thats checks users status
+//logged IN or NOT
+
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) {
+          return children;
+          //this will return signIn page
+        }
+
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
+
+//protection
+export function ProtectedRoute({ user, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: "signin",
+                state: { from: location },
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
