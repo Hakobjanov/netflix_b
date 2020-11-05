f6f5c2823ecd09b2163c420bb40195f68a465fc6
vahan akopjanian <vahan.bclub@gmail.com>
2020-11-05 17:06:27 +0200

    add use-content hook for films and series

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React from "react";
+import { useContent } from "../hooks/index";
 
 export default function Browse() {
-  return <h1>Browse motherfucker</h1>
+  // we need the series and the films
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+  console.log(series);
+  console.log(films);
+  // we also need slides
+  // pass them to browse container
+
+  return <h1>Browse motherfuckers</h1>;
 }
