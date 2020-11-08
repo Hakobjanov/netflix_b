af033cc8871ce2d60da322f04edd5f70188a1c06
vahan akopjanian <vahan.bclub@gmail.com>
2020-11-08 19:59:28 +0200

    created card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,9 @@
+import React, { useState, useContext, createContext } from "react";
+
+export const FeatureContext = createContext();
+
+export default function Card({ children, ...restProps }) {
+  const [showFeature, setShowFeature] = useState(false);
+
+  return <div></div>;
+}
